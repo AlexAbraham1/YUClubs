eed0cec6b960c748b4ab6cc069b4e897af9b2141
Alex Abraham <alex@abraham.net>
2015-03-30 20:24:18 -0400

    changed sql database data

--- src/main/java/configs/SQLConfig.java
+++ src/main/java/configs/SQLConfig.java
@@ -2,8 +2,8 @@ package main.java.configs;
 
 public class SQLConfig {
 
-    public static final String URL = "jdbc:mysql://127.0.0.1:8889/spark";
-    public static final String USER = "root";
-    public static final String PASS = "root";
+    public static final String URL = "jdbc:mysql://sql3.freemysqlhosting.net:3306/sql372372";
+    public static final String USER = "sql372372";
+    public static final String PASS = "qJ4!gM5*";
 
 }
