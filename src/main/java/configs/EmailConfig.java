c0633733f1770c6b7b56f0a100cdd2a8ba4b553d
Alex Abraham <alex@abraham.net>
2015-03-31 17:58:13 -0400

    testing multiple git locations

--- src/main/java/configs/EmailConfig.java
+++
@@ -1,7 +0,0 @@
-package main.java.configs;
-
-public class EmailConfig {
-
-    public static final String GMAIL_USER = "sparkjavatest@gmail.com";
-    public static final String GMAIL_PASS = "SparkTest123";
-}
--- src/main/java/configs/SQLConfig.java
+++
@@ -1,9 +0,0 @@
-package main.java.configs;
-
-public class SQLConfig {
-
-    public static final String URL = "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_db87cc3ef32195e?reconnect=true";
-    public static final String USER = "bbaaca2b59b0a6";
-    public static final String PASS = "fc29f656";
-
-}
